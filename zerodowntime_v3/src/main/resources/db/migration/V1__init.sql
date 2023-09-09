e0c258aa1c636aa63af9fd91bcddc6af841318b9
adrianrevilla009 <adrianrg1996@gmail.com>
2023-09-09 14:08:00 +0200

    v3

---
+++ zerodowntime_v3/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE person (
+	id BIGINT NOT NULL AUTO_INCREMENT, 
+	PRIMARY KEY(id),
+	first_name varchar(255) not null,
+	last_name varchar(255) not null,
+	gender varchar(255) not null
+);
+
+insert into person (first_name, last_name, gender) values ('Dave', 'Syer', 'Male');
